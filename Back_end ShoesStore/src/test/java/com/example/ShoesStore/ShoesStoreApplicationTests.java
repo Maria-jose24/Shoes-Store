1cb53abd0f0c928be550575bc3cb5263820a4f5d
unknown <mariajosemurciamartinez85@gmail.com>
2024-05-07 16:17:59 -0500

    Se sube lo del front_end

---
+++ Back_end ShoesStore/src/test/java/com/example/ShoesStore/ShoesStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShoesStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
